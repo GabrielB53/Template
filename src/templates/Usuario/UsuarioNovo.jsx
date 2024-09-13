fbdb69f55b49ed0f8c3e2dc9f2fe9c5776089e26
noelmeucu5@gmail.com <GabrielB53>
2024-09-13 09:15:58 -0300

    faz o L

--- src/templates/Usuario/UsuarioNovo.jsx
+++ src/templates/Usuario/UsuarioNovo.jsx
@@ -1,10 +1,26 @@
-import { Link } from "react-router-dom"
+import React, {useState, useEffect} from 'react';
 import Header from "../../components/Header/Header"
 import Sidebar from '../../components/Menu/Sidebar'
 import logo from '../../assets/images/home.png'
+import { Formik } from 'formik';
+import axios from 'axios';
 
 const UsuarioNovo = () => {
+    const [dados, setDados] = useState({})
+    const [clicou, setClicou] = useState(false)
 
+    function enviarDados(){
+        axios.post('http://localhost:8080/usuarionovo', 
+            dados
+        ).then(response => console.log(response))
+        .then(dados => alert('Dados enviados com sucesso'))
+        .catch(error => console.log(error))
+    }
+    
+    useEffect(()=>{
+       clicou ? enviarDados() : console.log('app no ar')
+       return (()=>setClicou(false))
+    }, [clicou])
     return (
         <div className="d-flex">
             <Sidebar />
@@ -15,31 +31,93 @@ const UsuarioNovo = () => {
                     logo={logo}
                 />
                 <section className="m-2 p-2 shadow-lg">
-                    <form className="row g-3">
-                        <div className="col-md-5">
-                            <label htmlFor="inputNome" className="form-label">Nome</label>
-                            <input type="text" className="form-control" id="inputNome" />
-                        </div>
-                        <div className="col-md-5">
-                            <label htmlFor="inputEmail4" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="inputEmail4" />
-                        </div>
-                       
-                        <div className="col-md-2">
-                            <label htmlFor="inputAcesso" className="form-label">Acesso</label>
-                            <select id="inputAcesso" className="form-select">
-                                <option selected>Nível de Acesso</option>
-                                <option>Aluno</option>
-                                <option>Administrador</option>
-                            </select>
-                        </div>
-                        
-                        <div className="col-12">
-                            <button type="submit" className="btn btn-primary">
-                                Gravar
-                            </button>
-                        </div>
-                    </form>
+            <Formik
+            initialValues={{
+                nome: '',
+                email: '',
+                destaque: '',
+                statusLog: 'Aluno'
+            }}
+            onSubmit={(values, actions) => {
+
+                if(values.nome.length > 0){
+                        setTimeout(() => {
+                        setDados({
+                            nome: values.nome,
+                            email: values.email,
+                            senha: values.senha,
+                            statusLog: values.statusLog
+                        })
+                        setClicou(true)
+                        // alert(JSON.stringify(values, null, 2));
+                        // console.log(JSON.stringify(values, null, 2));
+                        // actions.setSubmitting(false);
+                    }, 1000);
+                } else {
+                    alert('Favor preencher informações!')
+                }
+                
+            }}
+        >
+            {props => (
+                <form onSubmit={props.handleSubmit} className="row g-3">
+                    <div  className="col-md-5">
+                        <label htmlFor=""></label>
+                        <input
+                        className="form-control"
+                            type="text"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.nome}
+                            placeholder="Nome"
+                            name="nome"
+                        />
+                        {props.errors.nome && <div id="feedback">{props.errors.nome}</div>}
+                    </div>
+                    <div className="col-md-5"> 
+                        <input
+                        className="form-control"
+                            type="text"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.email}
+                            name="email"
+                            placeholder="0"
+                        />
+                        {props.errors.email && <div id="feedback">{props.errors.email}</div>}
+                    </div >
+                    <div className="col-md-5">
+                        <input
+                        className="form-control"
+                            type="text"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.senha}
+                            name="senha"
+                            placeholder="Senha"
+                        />
+                        {props.errors.senha && <div id="feedback">{props.errors.senha}</div>}
+                    </div>
+                    <div className="col-md-5">
+                        <select
+                        className="form-select"
+                            type="text"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.statusLog}
+                            name="statusLog"
+                        >
+                            <option>Aluno</option>
+                            <option>Funcionário</option>
+                        </select>
+                        {props.errors.statusLog && <div id="feedback">{props.errors.statusLog}</div>}
+                    </div>
+                    <div className="col-12">
+                    <button type="submit" className="btn btn-primary">SALVAR</button>
+                    </div>
+                </form>
+            )}
+        </Formik>
                 </section>
             </div>
         </div>
