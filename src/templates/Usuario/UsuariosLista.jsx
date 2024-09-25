a0318a87e15497b166ea889459dfb59d0c5cdd20
noelmeucu5@gmail.com <GabrielB53>
2024-09-25 09:15:59 -0300

    25.09.24

--- src/templates/Usuario/UsuariosLista.jsx
+++ src/templates/Usuario/UsuariosLista.jsx
@@ -1,47 +1,49 @@
-import { useNavigate} from "react-router-dom"
-import Header from "../../components/Header/Header"
-import Sidebar from '../../components/Menu/Sidebar'
-import logo from '../../assets/images/home.png'
-import axios from 'axios'
-import { useEffect, useState } from "react"
-
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Sidebar from '../../components/Menu/Sidebar';
+import logo from '../../assets/images/home.png';
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 const UsuariosLista = () => {
-    
     const navigate = useNavigate();
-    const editarUser = () => {
-        navigate('/usuarioeditar');  
-    };
-    const [dados, setDados] = useState([])
+    const [dados, setDados] = useState([]);
 
-    function receberDados(){
-        axios.get('http://localhost:8080/usuarionovo'
-        ).then(response => {
-            console.log(response.data)
-            setDados(response.data)
-        })
-        .catch(error => console.log(error))
-    }
-    useEffect(()=>{
-        receberDados()
-     }, [])
+    const editarUser = (id) => {
+        navigate(`/usuarioeditar/${id}`); // Passa o ID para a URL
+    };
+
+    const receberDados = () => {
+        axios.get('http://localhost:8080/usuarionovo')
+            .then(response => {
+                console.log(response.data);
+                setDados(response.data);
+            })
+            .catch(error => console.log(error));
+    };
+
+    useEffect(() => {
+        receberDados();
+    }, []);
 
     const ItensTable = () => dados.map(
-        usuarios => 
-        <tr key={usuarios.id} >
-            <td>{usuarios.id}</td>
-            <td>{usuarios.nome}</td>
-            <td>{usuarios.email}</td>
-            <td>{usuarios.tipoUsuario}</td>
-            <td><button
+        usuario => (
+            <tr key={usuario.id}>
+                <td>{usuario.id}</td>
+                <td>{usuario.nome}</td>
+                <td>{usuario.email}</td>
+                <td>{usuario.tipoUsuario}</td>
+                <td>
+                    <button
                         className="btn btn-danger"
-                        onClick={editarUser}
+                        onClick={() => editarUser(usuario.id)} // Passa o ID do usuário
                     >
                         Alterar
-            </button></td>
-        </tr>
-    )
-
+                    </button>
+                </td>
+            </tr>
+        )
+    );
 
     return (
         <div className="d-flex">
@@ -65,14 +67,14 @@ const UsuariosLista = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <ItensTable/>
+                                <ItensTable />
                             </tbody>
                         </table>
                     </div>
                 </section>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default UsuariosLista;
